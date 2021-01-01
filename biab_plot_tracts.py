f175b0c11956fbe8905c9c10ae2ba5b679979371
jellocube <jellocube@gmail.com>
2021-01-01 00:51:54 -0700

    Fixing tract plotting regex bug

--- biab_plot_tracts.py
+++ biab_plot_tracts.py
@@ -12,7 +12,7 @@ class Biab_PlotTracts(bpy.types.Operator):
         return tracts
 
     def getCoords3d(self, tract):
-        coords_list = re.findall('((\d+\.\d*?\s){3})', tract)       # get all 3D coordinates from text
+        coords_list = re.findall('(?!\d+\n)((\d+(\.\d*)?\s){3})', tract)       # get all 3D coordinates from text
         coords3d = []
         for i in range(0, len(coords_list)):
             coords = coords_list[i][0].split()
@@ -22,22 +22,18 @@ class Biab_PlotTracts(bpy.types.Operator):
                 if len(vector3d) == 2:
                     vector3d.append(coordsFloat)
                     coords3d.append(tuple((vector3d[0],vector3d[1],vector3d[2],)))
+                    print("Vector " + str(i) + ": " + str(vector3d))
                     vector3d = []
                 else:
                     vector3d.append(coordsFloat)
-        print(coords3d)
         return coords3d
 
     def makeCurve(self, tract):
         # create the curve
         curveData = bpy.data.curves.new('myCurve', type='CURVE')
         curveData.dimensions = '3D'
-        # curveData.resolution_u = 2            # what is this doing? expose?
         curveObj = curveData.splines.new('NURBS')
-        
         coords3d = self.getCoords3d(tract)
-        self.getCoords3d(tract)
-        #coords3d = [(1.5,1,1), (2,2.5,2), (1.2,2.1,1)]
 
         curveObj.points.add(len(coords3d))
         for i in coords3d:
@@ -49,12 +45,15 @@ class Biab_PlotTracts(bpy.types.Operator):
         tract = bpy.data.objects.new('Tract Curve', curveData)
         bpy.context.collection.objects.link(tract)
 
+        # finally, add tract to a collection by group, using a new function and regex search, so that this too runs once per tract.
+
 
     def execute(self, context):
         tract_file = open(context.scene.tract_file,"r")
         tracts = self.getTracts(tract_file)
 
         for t in tracts:
+            print("\nNow importing tract: " + str(tracts.index(t)))
             self.makeCurve(t)
 
         return {'FINISHED'}
