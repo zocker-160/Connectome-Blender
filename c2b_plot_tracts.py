8647a563d4335a9d60d1cecdb958eb71c93978d8
jellocube <jellocube@gmail.com>
2021-01-04 01:06:36 -0700

    Added feature: separate tract groups into collections

--- c2b_plot_tracts.py
+++ c2b_plot_tracts.py
@@ -6,26 +6,41 @@ class C2b_PlotTracts(bpy.types.Operator):
     bl_label = "Plot Tracts"
     bl_description = "Plot tracts from connectome source file in bezier curves"
 
-    groups = [0]
+    groups = []
     file_data = []
 
     def execute(self, context):
         tract_file = open(context.scene.tract_file,"r")
-        self.file_data = re.split(r"\n(\d)\n", tract_file.read())
+        self.file_data = re.split(r"\s\n(\d)\n?", tract_file.read())
+        self.file_data.pop() # remove last regex find because it's an empty group, see re.split documentation for more info
+        print(self.file_data)
 
-        bpy.ops.collection.create(name="Tract 0")
+        new_collection = []
 
         for d in self.file_data:
-            if len(str(d)) < 3:
+            if len(str(d)) <= 3:
                 if(int(d) not in self.groups):
+                    print("Group label " + str(int(d)) + " is not in group list " + str(self.groups))
                     self.groups.append(int(d))
-                    bpy.ops.collection.create(name="Tract " + str(d))
-                    #print("Creating collection.")
-                    #last_curve.collection_link(collection=self.groups[-1])
-                #else:
-                    #last_curve.collection_link(collection=self.groups[-1])
+                    
+                    print("Creating group collection.")
+
+                    new_collection = bpy.data.collections.new(name="Tract " + str(d))
+                    bpy.context.scene.collection.children.link(new_collection) 
+                    new_collection.objects.link(current_curve)
+                    bpy.context.collection.objects.unlink(current_curve) # remove from default collection
+
+                    print("Added " + str(current_curve) + " to " + str(new_collection))
+
+                else:
+                    new_collection.objects.link(current_curve)
+                    bpy.context.collection.objects.unlink(current_curve) # remove from default collection
+                    print("Added " + str(current_curve) + " to " + str(new_collection))
+
             else:
-                last_curve = self.makeCurve(d)
+                context.view_layer.objects.active = self.makeCurve(d)
+                current_curve = context.view_layer.objects.active
+                
 
         self.updateCalculations(context)
 
@@ -46,8 +61,8 @@ class C2b_PlotTracts(bpy.types.Operator):
                 curveObj.points[i].co = (x, y, z, 1)
 
         curve = bpy.data.objects.new('Tract Curve', curveData)
-        bpy.context.collection.objects.link(curve)         #deprecating this
-
+        bpy.context.collection.objects.link(curve) # add to default collection for active object tracking, is removed in next steps
+        
         return curve
 
     def getTractCoords(self, tract):
@@ -62,7 +77,7 @@ class C2b_PlotTracts(bpy.types.Operator):
                 if len(vertex) == 2:
                     vertex.append(coordsFloat)
                     tract_vertices.append(tuple((vertex[0],vertex[1],vertex[2],)))
-                    #print("Vector " + str(i) + ": " + str(vertex))
+                    print("Vector " + str(i) + ": " + str(vertex))
                     vertex = []
                 else:
                     vertex.append(coordsFloat)
